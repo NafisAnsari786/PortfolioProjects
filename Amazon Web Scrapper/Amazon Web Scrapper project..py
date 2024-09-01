cd6145a5a7423e114ec55559da98c07032da975c
NafisAnsari786 <94855409+NafisAnsari786@users.noreply.github.com>
2024-09-01 23:05:28 +0530

    Update Amazon Web Scrapper project..py

--- Amazon Web Scrapper/Amazon Web Scrapper project..py
+++ Amazon Web Scrapper/Amazon Web Scrapper project..py
@@ -15,7 +15,7 @@ URL='https://www.amazon.in/gp/product/B0CHX2WQLX/ref=s9_bw_cg_Budget_8f1_w?pf_rd
 
 headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36","Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
-page=r.get(URL, headers=headers)
+page=r.get(URL, headers=headers) 
 
 soup1=BeautifulSoup(page.content, 'html.parser')
 print(soup1)  
